822194fc9cb72ccfd14b84c5280a42c39a81d650
Ricardo Galan <devricardogalan@gmail.com>
2019-04-09 22:39:33 -0300

    Routing with Home and About

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchBarComponent } from './components/SearchBar/searchbar.component';
+import { AboutComponent } from './components/About/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: SearchBarComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
